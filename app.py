dae83cc243f613f2e7940c37d9946800393b02fb
alxlgogo <alxlgogo@gmail.com>
2022-03-07 18:54:19 +0000

    add login function and login.html

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, flash
 from flask_mysqldb import MySQL
+import pymysql.cursors
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'root1234'
 app.config['MYSQL_DB'] = 'assignment_db'
 
 mysql = MySQL(app)
+
 
 @app.route('/new_user')
 def add_user_view():
     return render_template('register.html')
 
+
 @app.route('/register', methods=['post'])
 def add_user():
     conn = None
     cursor = None
-    try: 
+    try:
         details = request.form
         first_name = details['first_name']
         last_name = details['last_name']
@@ -34,7 +37,7 @@ def add_user():
             cursor.execute(sql, data)
             conn.commit()
             response = jsonify(message='User added successfully.', id=cursor.lastrowid)
-            #response.data = cursor.lastrowid
+            # response.data = cursor.lastrowid
             response.status_code = 200
             flash('User added successfully!')
             return redirect('/home')
@@ -42,19 +45,21 @@ def add_user():
             return 'Error while adding user'
     except Exception as e:
         print(e)
-        response = jsonify('Failed to add user.')         
-        response.status_code = 400 
+        response = jsonify('Failed to add user.')
+        response.status_code = 400
     finally:
-        cursor.close() 
+        cursor.close()
         conn.close()
-        return(response)    
+        return (response)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/error')
-def error_handle(e):  
+def error_handle(e):
     return e
 
 
@@ -65,3 +70,41 @@ if __name__ == '__main__':
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/go_to_login')
+def go_to_login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user_name = request.form['user_name']
+    password = request.form['password']
+    get_tus_user(user_name, password)
+    return render_template('login.html')
+
+
+def get_tus_user(username, password):
+    connect = pymysql.Connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        passwd='root1234',
+        db='assignment_db'
+    )
+
+    cursor = connect.cursor()
+    sql = "SELECT * FROM assignment_db.tus_user where first_name='" + username + "' and password=" + password + "; "
+    cursor.execute(sql)
+    user = cursor.fetchall()
+    print(user)
+    # for row in cursor.fetchall():
+    #     id = row[1]
+    #     username = row[2]
+    #     last_name = row[3]
+    #     email = row[4]
+    #     password = row[5]
+    #     print(row)
+    connect.close()
+    return user
