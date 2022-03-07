09e88f1b5a1056dda5d20871392af5414f1519a3
alxlgogo <alxlgogo@gmail.com>
2022-03-07 19:34:28 +0000

    Add a field describe into permission table. Add a field user_name into tus_user table. Add user root as admin into tus_user, password is root.

--- app.py
+++ app.py
@@ -1,56 +1,56 @@
 from flask import Flask, render_template, request, redirect, jsonify, flash
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 import pymysql.cursors
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root1234'
-app.config['MYSQL_DB'] = 'assignment_db'
-
-mysql = MySQL(app)
-
-
-@app.route('/new_user')
-def add_user_view():
-    return render_template('register.html')
-
-
-@app.route('/register', methods=['post'])
-def add_user():
-    conn = None
-    cursor = None
-    try:
-        details = request.form
-        first_name = details['first_name']
-        last_name = details['last_name']
-        email = details['email']
-        password = details['password']
-        # validate the received values
-        if first_name and last_name and email and request.method == 'POST':
-            # save edits
-            sql = "INSERT INTO tus_user(first_name, last_name, email, password) VALUES(%s, %s, %s, %s)"
-            data = (first_name, last_name, email, password)
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(sql, data)
-            conn.commit()
-            response = jsonify(message='User added successfully.', id=cursor.lastrowid)
-            # response.data = cursor.lastrowid
-            response.status_code = 200
-            flash('User added successfully!')
-            return redirect('/home')
-        else:
-            return 'Error while adding user'
-    except Exception as e:
-        print(e)
-        response = jsonify('Failed to add user.')
-        response.status_code = 400
-    finally:
-        cursor.close()
-        conn.close()
-        return (response)
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'root1234'
+# app.config['MYSQL_DB'] = 'assignment_db'
+#
+# mysql = MySQL(app)
+#
+#
+# @app.route('/new_user')
+# def add_user_view():
+#     return render_template('register.html')
+#
+#
+# @app.route('/register', methods=['post'])
+# def add_user():
+#     conn = None
+#     cursor = None
+#     try:
+#         details = request.form
+#         first_name = details['first_name']
+#         last_name = details['last_name']
+#         email = details['email']
+#         password = details['password']
+#         # validate the received values
+#         if first_name and last_name and email and request.method == 'POST':
+#             # save edits
+#             sql = "INSERT INTO tus_user(first_name, last_name, email, password) VALUES(%s, %s, %s, %s)"
+#             data = (first_name, last_name, email, password)
+#             conn = mysql.connect()
+#             cursor = conn.cursor()
+#             cursor.execute(sql, data)
+#             conn.commit()
+#             response = jsonify(message='User added successfully.', id=cursor.lastrowid)
+#             # response.data = cursor.lastrowid
+#             response.status_code = 200
+#             flash('User added successfully!')
+#             return redirect('/home')
+#         else:
+#             return 'Error while adding user'
+#     except Exception as e:
+#         print(e)
+#         response = jsonify('Failed to add user.')
+#         response.status_code = 400
+#     finally:
+#         cursor.close()
+#         conn.close()
+#         return (response)
 
 
 @app.route('/')
--- doc/assignment_db.sql
+++ doc/assignment_db.sql
@@ -1,69 +1,249 @@
-CREATE TABLE tus_user (
-user_id VARCHAR (40) NOT NULL,
-first_name VARCHAR(20),
-last_name VARCHAR(20),
-email  VARCHAR(40),
-PRIMARY KEY (user_id)
-);
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+--
+-- Host: localhost    Database: assignment_db
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE grade (
-grade_id VARCHAR (40),
-assessment_mark DOUBLE,
-exam_mark DOUBLE,
-final_mark DOUBLE,
-PRIMARY KEY (grade_id)
-);
+--
+-- Table structure for table `grade`
+--
 
-CREATE TABLE module (
-module_id VARCHAR (40),
-module_name VARCHAR(40),
-PRIMARY KEY (module_id)
-);
+DROP TABLE IF EXISTS `grade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grade` (
+  `grade_id` varchar(40) NOT NULL,
+  `assessment_mark` double DEFAULT NULL,
+  `exam_mark` double DEFAULT NULL,
+  `final_mark` double DEFAULT NULL,
+  PRIMARY KEY (`grade_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE role (
-role_id VARCHAR (40),
-role_type VARCHAR(20),
-PRIMARY KEY (role_id)
-);
+--
+-- Dumping data for table `grade`
+--
 
-CREATE TABLE permission (
-permission_id VARCHAR (40),
-url VARCHAR(40),
-PRIMARY KEY (permission_id)
-);
+LOCK TABLES `grade` WRITE;
+/*!40000 ALTER TABLE `grade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grade` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE role_permission (
-role_permission_id VARCHAR(20) NOT NULL,
-role_id VARCHAR (40),
-permission_id VARCHAR (40),
-PRIMARY KEY (role_permission_id),
-FOREIGN KEY (role_id) REFERENCES role(role_id),
-FOREIGN KEY (permission_id) REFERENCES permission(permission_id)
-);
+--
+-- Table structure for table `module`
+--
 
-CREATE TABLE user_role (
-user_role_id VARCHAR(20) NOT NULL, 
-user_id VARCHAR (40),
-role_id VARCHAR (40),
-PRIMARY KEY (user_role_id),
-FOREIGN KEY (user_id) REFERENCES tus_user(user_id),
-FOREIGN KEY (role_id) REFERENCES role(role_id)
-);
+DROP TABLE IF EXISTS `module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `module` (
+  `module_id` varchar(40) NOT NULL,
+  `module_name` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`module_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE user_module (
-user_module_id VARCHAR(20) NOT NULL,
-user_id VARCHAR (40),
-module_id VARCHAR (40),
-grade_id VARCHAR (40),
-PRIMARY KEY (user_module_id),
-FOREIGN KEY (user_id) REFERENCES tus_user(user_id),
-FOREIGN KEY (module_id) REFERENCES module(module_id),
-FOREIGN KEY (grade_id) REFERENCES grade(grade_id)
-);
+--
+-- Dumping data for table `module`
+--
 
-ALTER TABLE tus_user
-ADD password VARCHAR(20);
+LOCK TABLES `module` WRITE;
+/*!40000 ALTER TABLE `module` DISABLE KEYS */;
+/*!40000 ALTER TABLE `module` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `permission`
+--
 
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permission` (
+  `permission_id` varchar(40) NOT NULL,
+  `url` varchar(40) DEFAULT NULL,
+  `describe` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+INSERT INTO `permission` VALUES ('1','/register','register user'),('2','/login','login user');
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `role_id` varchar(40) NOT NULL,
+  `role_type` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES ('1','admin'),('2','lecturer'),('3','student');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_permission`
+--
+
+DROP TABLE IF EXISTS `role_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_permission` (
+  `role_permission_id` varchar(20) NOT NULL,
+  `role_id` varchar(40) DEFAULT NULL,
+  `permission_id` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`role_permission_id`),
+  KEY `role_id` (`role_id`),
+  KEY `permission_id` (`permission_id`),
+  CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
+  CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_permission`
+--
+
+LOCK TABLES `role_permission` WRITE;
+/*!40000 ALTER TABLE `role_permission` DISABLE KEYS */;
+INSERT INTO `role_permission` VALUES ('1','1','1'),('2','1','2'),('3','2','1'),('4','2','2'),('5','3','1'),('6','3','2');
+/*!40000 ALTER TABLE `role_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tus_user`
+--
+
+DROP TABLE IF EXISTS `tus_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tus_user` (
+  `user_id` varchar(40) NOT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tus_user`
+--
+
+LOCK TABLES `tus_user` WRITE;
+/*!40000 ALTER TABLE `tus_user` DISABLE KEYS */;
+INSERT INTO `tus_user` VALUES ('1','root','tom','lee','tom@gmail.com','root');
+/*!40000 ALTER TABLE `tus_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_module`
+--
+
+DROP TABLE IF EXISTS `user_module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_module` (
+  `user_module_id` varchar(20) NOT NULL,
+  `user_id` varchar(40) DEFAULT NULL,
+  `module_id` varchar(40) DEFAULT NULL,
+  `grade_id` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`user_module_id`),
+  KEY `user_id` (`user_id`),
+  KEY `module_id` (`module_id`),
+  KEY `grade_id` (`grade_id`),
+  CONSTRAINT `user_module_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tus_user` (`user_id`),
+  CONSTRAINT `user_module_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `module` (`module_id`),
+  CONSTRAINT `user_module_ibfk_3` FOREIGN KEY (`grade_id`) REFERENCES `grade` (`grade_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_module`
+--
+
+LOCK TABLES `user_module` WRITE;
+/*!40000 ALTER TABLE `user_module` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_module` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role` (
+  `user_role_id` varchar(20) NOT NULL,
+  `user_id` varchar(40) DEFAULT NULL,
+  `role_id` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`user_role_id`),
+  KEY `user_id` (`user_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tus_user` (`user_id`),
+  CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES ('1','1','1');
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'assignment_db'
+--
+
+--
+-- Dumping routines for database 'assignment_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-07 19:24:48
