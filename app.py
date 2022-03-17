3d19feccf2bb15eda378fd4ca5a189d8c18b2067
alxlgogo <alxlgogo@gmail.com>
2022-03-17 22:07:56 +0000

    Add user list and pagination functions. Add user.css, user.html.

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import Flask, render_template, request, redirect, jsonify, flash
 from flask_mysqldb import MySQL
 import pymysql.cursors
 from models.tus_user import tus_user
-from service.user import get_user
+from service.user import get_user, insert_users_service
+from flask_paginate import Pagination, get_page_parameter, get_page_args
+
+pageLimit = 10
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
@@ -15,47 +18,47 @@ app.config['MYSQL_DB'] = 'assignment_db'
 mysql = MySQL(app)
 
 
-#
-#
-# @app.route('/new_user')
-# def add_user_view():
-#     return render_template('register.html')
-#
-#
-# @app.route('/register', methods=['post'])
-# def add_user():
-#     conn = None
-#     cursor = None
-#     try:
-#         details = request.form
-#         first_name = details['first_name']
-#         last_name = details['last_name']
-#         email = details['email']
-#         password = details['password']
-#         # validate the received values
-#         if first_name and last_name and email and request.method == 'POST':
-#             # save edits
-#             sql = "INSERT INTO tus_user(first_name, last_name, email, password) VALUES(%s, %s, %s, %s)"
-#             data = (first_name, last_name, email, password)
-#             conn = mysql.connect()
-#             cursor = conn.cursor()
-#             cursor.execute(sql, data)
-#             conn.commit()
-#             response = jsonify(message='User added successfully.', id=cursor.lastrowid)
-#             # response.data = cursor.lastrowid
-#             response.status_code = 200
-#             flash('User added successfully!')
-#             return redirect('/home')
-#         else:
-#             return 'Error while adding user'
-#     except Exception as e:
-#         print(e)
-#         response = jsonify('Failed to add user.')
-#         response.status_code = 400
-#     finally:
-#         cursor.close()
-#         conn.close()
-#         return (response)
+
+
+@app.route('/new_user')
+def add_user_view():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['post'])
+def add_user():
+    conn = None
+    cursor = None
+    try:
+        details = request.form
+        first_name = details['first_name']
+        last_name = details['last_name']
+        email = details['email']
+        password = details['password']
+        # validate the received values
+        if first_name and last_name and email and request.method == 'POST':
+            # save edits
+            sql = "INSERT INTO tus_user(first_name, last_name, email, password) VALUES(%s, %s, %s, %s)"
+            data = (first_name, last_name, email, password)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            response = jsonify(message='User added successfully.', id=cursor.lastrowid)
+            # response.data = cursor.lastrowid
+            response.status_code = 200
+            flash('User added successfully!')
+            return redirect('/home')
+        else:
+            return 'Error while adding user'
+    except Exception as e:
+        print(e)
+        response = jsonify('Failed to add user.')
+        response.status_code = 400
+    finally:
+        cursor.close()
+        conn.close()
+        return (response)
 
 
 @app.route('/')
@@ -86,6 +89,37 @@ def go_to_login():
 def login():
     user_name = request.form['user_name']
     password = request.form['password']
-    rv = get_user(mysql, password, user_name)
+    users = get_user(mysql, password, user_name)
+    if len(users) > 0:
+        user = users[0]
     msg = "username or password is wrong"
-    return render_template('login.html', user_name=user_name, user=rv[0])
+    return render_template('user.html', user_name=user_name, user=user)
+
+
+@app.route('/users', methods=['GET'])
+def users():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    offset = str((page - 1) * 10)
+    cur = mysql.connection.cursor()
+    sql = "SELECT * FROM assignment_db.tus_user order by user_id desc;"
+    cur.execute(sql)
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    pagination = Pagination(page=page, per_page=per_page, total=len(rv))
+    # json_data = []
+    # for result in rv:
+    #     json_data.append(dict(zip(row_headers, result)))
+
+    #
+    sql = "SELECT * FROM assignment_db.tus_user limit 10 OFFSET " + offset + ";"
+    cur.execute(sql)
+    row_headers = [x[0] for x in cur.description]
+    rv2 = cur.fetchall()
+    pagination_users = rv2
+    return render_template('user.html', users=pagination_users, per_page=per_page, rv=rv2, pagination=pagination)
+
+
+@app.route('/insert_users', methods=['GET'])
+def insert_users():
+    insert_users_service(mysql)
+    return "1"
--- service/user.py
+++ service/user.py
@@ -10,4 +10,30 @@ def get_user(mysql, password, user_name):
     json_data = []
     for result in rv:
         json_data.append(dict(zip(row_headers, result)))
-    return json.dumps(json_data)
+    # return json.dumps(json_data)
+    return json_data
+
+
+def get_users(mysql):
+    cur = mysql.connection.cursor()
+    cur.execute(
+        # "SELECT * FROM assignment_db.tus_user;")
+        "SELECT * FROM assignment_db.tus_user order by user_id desc limit 10;")
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    return json_data
+
+
+def insert_users_service(mysql):
+    cur = mysql.connection.cursor()
+    for x in range(1, 50):
+        id = str(x + 1)
+        user_name = "tom" + id
+        email = user_name + "@gmail.com"
+        sql = "insert into assignment_db.tus_user values("+id+",'"+user_name+"','','','"+email+"','"+user_name+"')"
+        print(sql)
+        cur.execute(sql)
+        mysql.connection.commit()
